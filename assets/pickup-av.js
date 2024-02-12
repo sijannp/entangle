32254571261763043a50cb2eea5c13d5b43165b3
sijann <sijannpaudel@gmail.com>
2024-02-13 00:10:56 +0545

    Pickup avaulability styles

--- assets/pickup-av.js
+++ assets/pickup-av.js
@@ -63,59 +63,59 @@ if (!customElements.get('pickup-availability')) {
                     document.querySelector('pickup-availability-drawer').classList.add(colorClass);
                 });
 
-                const button = this.querySelector('button');
-                if (button)
-                    button.addEventListener('click', (evt) => {
-                        document.querySelector('pickup-availability-drawer').show(evt.target);
-                    });
+                // const button = this.querySelector('button');
+                // if (button)
+                //     button.addEventListener('click', (evt) => {
+                //         document.querySelector('pickup-availability-drawer').show(evt.target);
+                //     });
             }
         }
     );
 }
 
-if (!customElements.get('pickup-availability-drawer')) {
-    customElements.define(
-        'pickup-availability-drawer',
-        class PickupAvailabilityDrawer extends HTMLElement {
-            constructor() {
-                super();
+// if (!customElements.get('pickup-availability-drawer')) {
+//     customElements.define(
+//         'pickup-availability-drawer',
+//         class PickupAvailabilityDrawer extends HTMLElement {
+//             constructor() {
+//                 super();
 
-                this.onBodyClick = this.handleBodyClick.bind(this);
+//                 this.onBodyClick = this.handleBodyClick.bind(this);
 
-                this.querySelector('button').addEventListener('click', () => {
-                    this.hide();
-                });
+//                 this.querySelector('button').addEventListener('click', () => {
+//                     this.hide();
+//                 });
 
-                this.addEventListener('keyup', (event) => {
-                    if (event.code.toUpperCase() === 'ESCAPE') this.hide();
-                });
-            }
+//                 this.addEventListener('keyup', (event) => {
+//                     if (event.code.toUpperCase() === 'ESCAPE') this.hide();
+//                 });
+//             }
 
-            handleBodyClick(evt) {
-                const target = evt.target;
-                if (
-                    target != this &&
-                    !target.closest('pickup-availability-drawer') &&
-                    target.id != 'ShowPickupAvailabilityDrawer'
-                ) {
-                    this.hide();
-                }
-            }
+//             handleBodyClick(evt) {
+//                 const target = evt.target;
+//                 if (
+//                     target != this &&
+//                     !target.closest('pickup-availability-drawer') &&
+//                     target.id != 'ShowPickupAvailabilityDrawer'
+//                 ) {
+//                     this.hide();
+//                 }
+//             }
 
-            hide() {
-                this.removeAttribute('open');
-                document.body.removeEventListener('click', this.onBodyClick);
-                document.body.classList.remove('overflow-hidden');
-                removeTrapFocus(this.focusElement);
-            }
+//             hide() {
+//                 this.removeAttribute('open');
+//                 document.body.removeEventListener('click', this.onBodyClick);
+//                 document.body.classList.remove('overflow-hidden');
+//                 removeTrapFocus(this.focusElement);
+//             }
 
-            show(focusElement) {
-                this.focusElement = focusElement;
-                this.setAttribute('open', '');
-                document.body.addEventListener('click', this.onBodyClick);
-                document.body.classList.add('overflow-hidden');
-                trapFocus(this);
-            }
-        }
-    );
-}
+//             show(focusElement) {
+//                 this.focusElement = focusElement;
+//                 this.setAttribute('open', '');
+//                 document.body.addEventListener('click', this.onBodyClick);
+//                 document.body.classList.add('overflow-hidden');
+//                 trapFocus(this);
+//             }
+//         }
+//     );
+// }
